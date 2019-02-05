4dadd3419f524f72865d376ef33561d8268c45b4
jduncan <jduncan@redhat.com>
2019-02-05 15:38:01 -0500

    fixing indentation errors

--- 1_2_5-define_openshift_environment.sh
+++ 1_2_5-define_openshift_environment.sh
@@ -2,53 +2,53 @@
 # Create the openshift_env.yaml file
 cat << EOF > $DOCS_DIR/openshift_env.yaml
 parameter_defaults:
-# by default Director assigns the VIP random from the allocation pool
-# by using the FixedIPs we can set the VIPs to predictable IPs before starting the deployment
+  # by default Director assigns the VIP random from the allocation pool
+  # by using the FixedIPs we can set the VIPs to predictable IPs before starting the deployment
 
-CloudName: openshift.localdomain
-PublicVirtualFixedIPs: [{'ip_address':'192.168.122.100'}]
+  CloudName: openshift.localdomain
+  PublicVirtualFixedIPs: [{'ip_address':'192.168.122.100'}]
 
-CloudNameInternal: internal.openshift.localdomain
-InternalApiVirtualFixedIPs: [{'ip_address':'172.17.1.100'}]
+  CloudNameInternal: internal.openshift.localdomain
+  InternalApiVirtualFixedIPs: [{'ip_address':'172.17.1.100'}]
 
-CloudDomain: openshift.localdomain
+  CloudDomain: openshift.localdomain
 
-## Required for CNS deployments only
+  ## Required for CNS deployments only
   OpenShiftInfraParameters:
     OpenShiftGlusterDisks:
       - /dev/vdb
 
-## Required for CNS deployments only
+  ## Required for CNS deployments only
   OpenShiftWorkerParameters:
     OpenShiftGlusterDisks:
       - /dev/vdb
       - /dev/vdc
 
-NtpServer: ["clock.redhat.com","clock2.redhat.com"]
+  NtpServer: ["clock.redhat.com","clock2.redhat.com"]
 
-ControlPlaneDefaultRoute: 192.168.122.1
-EC2MetadataIp: 192.168.122.253
-ControlPlaneSubnetCidr: 24
+  ControlPlaneDefaultRoute: 192.168.122.1
+  EC2MetadataIp: 192.168.122.253
+  ControlPlaneSubnetCidr: 24
 
-# The DNS server below should have entries for resolving {internal,public,apps}.openshift.localdomain names
-DnsServers:
-   - 172.16.0.21
+  # The DNS server below should have entries for resolving {internal,public,apps}.openshift.localdomain names
+  DnsServers:
+     - 172.16.0.21
 
-OpenShiftGlobalVariables:
+  OpenShiftGlobalVariables:
 
-    openshift_master_identity_providers:
-    - name: 'htpasswd_auth'
-      login: 'true'
-      challenge: 'true'
-      kind: 'HTPasswdPasswordIdentityProvider'
-    openshift_master_htpasswd_users:
-      sysadmin: '$apr1$jpBOUqeU$X4jUsMyCHOOp8TFYtPq0v1'
+      openshift_master_identity_providers:
+      - name: 'htpasswd_auth'
+        login: 'true'
+        challenge: 'true'
+        kind: 'HTPasswdPasswordIdentityProvider'
+      openshift_master_htpasswd_users:
+        sysadmin: '$apr1$jpBOUqeU$X4jUsMyCHOOp8TFYtPq0v1'
 
-    #openshift_master_cluster_hostname should match the CloudNameInternal parameter
-    openshift_master_cluster_hostname: internal.openshift.localdomain
+      #openshift_master_cluster_hostname should match the CloudNameInternal parameter
+      openshift_master_cluster_hostname: internal.openshift.localdomain
 
-    #openshift_master_cluster_public_hostname should match the CloudName parameter
-    openshift_master_cluster_public_hostname: public.openshift.localdomain
+      #openshift_master_cluster_public_hostname should match the CloudName parameter
+      openshift_master_cluster_public_hostname: public.openshift.localdomain
 
-    openshift_master_default_subdomain: apps.openshift.localdomain
+      openshift_master_default_subdomain: apps.openshift.localdomain
 EOF
